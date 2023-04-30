d8af8343d94378d52f24a07235a1720f7ddb2aec
KaleemAhmed123 <shaamidreez@gmail.com>
2023-04-30 13:58:17 +0530

    initial navabar done.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import "./navbar.css";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="container">
+      <div className="navbar">
+        <div className="nav-items">
+          <span className="item">Home </span>
+          <span className="item">About </span>
+          <span className="item">Features</span>
+          {/*  down icon left */}
+          <span className="item">Pages </span>
+          <span className="item">Blog</span>
+          <span className="item"> Cart (0)</span>
+        </div>
+      </div>
+      <button className="btn btn-gray util">Sign In</button>
+    </nav>
+  );
 };
 
 export default Navbar;
